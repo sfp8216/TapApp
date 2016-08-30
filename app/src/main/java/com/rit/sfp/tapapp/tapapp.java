b3b0148f9382a3b5c04cc586540a702d9de4d4ff
Steven Pantaleo <Steven Pantaleo>
2016-08-30 16:32:44 -0400

    TapApp v1

--- app/src/main/java/com/rit/sfp/tapapp/tapapp.java
+++ app/src/main/java/com/rit/sfp/tapapp/tapapp.java
@@ -2,6 +2,8 @@ package com.rit.sfp.tapapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class tapapp extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class tapapp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tapapp);
+    }
+
+    //Toast is a popup window
+    //Length_Long makes the toast stay for a while
+    public void onButtonTap(View v){
+        Toast myToast = Toast.makeText(getApplicationContext(), "Ouch!",Toast.LENGTH_LONG);
+
+        //To get the popup to show up
+        myToast.show();
     }
 }
