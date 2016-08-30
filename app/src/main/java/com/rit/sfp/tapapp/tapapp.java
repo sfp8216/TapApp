a11112d3cdd974d10c706ce8c2bf47b34ba7df23
Steven Pantaleo <Steven Pantaleo>
2016-08-30 16:08:55 -0400

    Initial Commiqt

---
+++ app/src/main/java/com/rit/sfp/tapapp/tapapp.java
@@ -0,0 +1,13 @@
+package com.rit.sfp.tapapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tapapp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tapapp);
+    }
+}
